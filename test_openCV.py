a8a7c34038e14442281dac77e20d6411ead9d7bd
FateFaker <877814399@qq.com>
2017-06-28 14:52:14 +0800

    no message

--- test_openCV.py
+++ test_openCV.py
@@ -2,7 +2,7 @@
 
 import cv2
 import sys
-from PIL import Image
+# from PIL import Image
 
 def CatchUsbVideo(window_name, camera_idx):
     cv2.namedWindow(window_name)
@@ -20,7 +20,7 @@ def CatchUsbVideo(window_name, camera_idx):
         c = cv2.waitKey(10)
         if c & 0xFF == ord('q'):
             break        
-    
+
     #释放摄像头并销毁所有窗口
     cap.release()
     cv2.destroyAllWindows() 
