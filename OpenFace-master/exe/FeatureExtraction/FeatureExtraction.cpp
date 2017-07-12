546a52420e2266234384e748b73174f831d6ea53
张轩 <riolu@vip.qq.com>
2017-07-12 15:44:12 +0800

    更新输出框架

--- OpenFace-master/exe/FeatureExtraction/FeatureExtraction.cpp
+++ OpenFace-master/exe/FeatureExtraction/FeatureExtraction.cpp
@@ -1176,8 +1176,6 @@ void outputAllFeatures(std::ofstream* output_file, bool output_2D_landmarks, boo
 
 		    close(sockfd);
 
-		    cout << pose_estimate[3] << "  " << pose_estimate[4] << "  " << pose_estimate[5] << endl;
-		  
 			//test
 			// for(int j = 0;j<=3;j++){
 			// 	for(int i = 0;i<=8;i++){
@@ -1197,6 +1195,10 @@ void outputAllFeatures(std::ofstream* output_file, bool output_2D_landmarks, boo
 
 	}
 	*output_file << endl;
+
+	cout << pose_estimate[3] << "  " << pose_estimate[4] << "  " << pose_estimate[5] << endl;
+	cout << "点头：" << (nodding ? "是" : "否") << endl;
+	cout << "摇头：" << (shaking ? "是" : "否") << endl;
 }
 
 
